a7686799ff99b4dc62fcfa57089ba06559cd4cb4
Chaitanya2267 <chaitanya22607@gmail.com>
2026-02-27 01:56:15 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,19 @@
+package LibraryManagement;
+
+public class Book {
+    String title;
+    String author;
+    String ISBN;
+
+    public Book(String title, String author, String ISBN) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+    }
+
+    public void displayBook() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("ISBN: " + ISBN);
+    }
+}
